03ba4e2936988e49b70f60599999f4dca0cfb74e
Will <willgoldby@google.com>
2022-07-08 18:12:13 +0000

    Add hello world files

---
+++ Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        String name = in.next();
+        System.out.println("Hello, " + name + "!");
+    
+    }
+}
